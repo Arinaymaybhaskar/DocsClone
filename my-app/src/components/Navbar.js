7c127d5c1806c56b0a1cae9422b9176ef50ce4dd
Arinaymay Bhaskar <arinaymay.bhaskar.7500@gmail.com>
2023-05-14 22:14:47 +0530

    frontend index

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+     Navbar
+    </>
+  );
+};
+export default Navbar;
---
+++ my-app/src/components/textEditor.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+const textEditor = () => {
+  return (
+    <>
+     Text Editor
+    </>
+  );
+};
+export default textEditor;
